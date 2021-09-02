0a72ddcdeb551829c438fa150bb389cd51120c21
Tobias Lindberg <tobias.lindberg@ga.ntig.se>
2021-09-02 11:30:04 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Exempel_1._1
+{
+    partial class frmExempel1_1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnKör = new System.Windows.Forms.Button();
+            this.tbxTal1 = new System.Windows.Forms.TextBox();
+            this.tbxTal2 = new System.Windows.Forms.TextBox();
+            this.tbxSumma = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnKör
+            // 
+            this.btnKör.Location = new System.Drawing.Point(143, 88);
+            this.btnKör.Name = "btnKör";
+            this.btnKör.Size = new System.Drawing.Size(75, 23);
+            this.btnKör.TabIndex = 0;
+            this.btnKör.Text = "Kör";
+            this.btnKör.UseVisualStyleBackColor = true;
+            this.btnKör.Click += new System.EventHandler(this.btnKör_Click);
+            // 
+            // tbxTal1
+            // 
+            this.tbxTal1.Location = new System.Drawing.Point(62, 43);
+            this.tbxTal1.Name = "tbxTal1";
+            this.tbxTal1.Size = new System.Drawing.Size(100, 20);
+            this.tbxTal1.TabIndex = 1;
+            // 
+            // tbxTal2
+            // 
+            this.tbxTal2.Location = new System.Drawing.Point(206, 43);
+            this.tbxTal2.Name = "tbxTal2";
+            this.tbxTal2.Size = new System.Drawing.Size(100, 20);
+            this.tbxTal2.TabIndex = 2;
+            // 
+            // tbxSumma
+            // 
+            this.tbxSumma.Location = new System.Drawing.Point(130, 143);
+            this.tbxSumma.Name = "tbxSumma";
+            this.tbxSumma.Size = new System.Drawing.Size(100, 20);
+            this.tbxSumma.TabIndex = 3;
+            // 
+            // frmExempel1_1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(368, 224);
+            this.Controls.Add(this.tbxSumma);
+            this.Controls.Add(this.tbxTal2);
+            this.Controls.Add(this.tbxTal1);
+            this.Controls.Add(this.btnKör);
+            this.Name = "frmExempel1_1";
+            this.Text = "Exempel 1.1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKör;
+        private System.Windows.Forms.TextBox tbxTal1;
+        private System.Windows.Forms.TextBox tbxTal2;
+        private System.Windows.Forms.TextBox tbxSumma;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exempel_1._1
+{
+    public partial class frmExempel1_1 : Form
+    {
+
+        /* Gjord av Tobias Lindberg 2021-08-30 */
+        public frmExempel1_1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKör_Click(object sender, EventArgs e)
+        {
+            //Omvandla till int
+            int tal1 = ParseToInt(tbxTal1.Text);
+            int tal2 = ParseToInt(tbxTal2.Text);
+
+            //Addera
+            int summa = tal1 + tal2;
+
+            //Omvandla summan till sträng och visa svaret
+            tbxSumma.Text = summa.ToString();
+        }
+
+        private int ParseToInt(string text)
+        {
+            int tal = 0;
+
+            //Börja med värdet av entalet
+            int positionsvärde = 1;
+            
+            //"Parsa" texten och beräkna tal
+            for(int i = text.Length-1; i >= 0; i--)
+            {
+                int teckenvärde = text[i] - 48;
+                tal += teckenvärde * positionsvärde;
+                positionsvärde *= 10;
+            }
+
+            return tal;
+        }
+    }
+
+
+}
